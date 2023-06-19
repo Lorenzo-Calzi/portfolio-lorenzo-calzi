b35ed91f3922bb78fa179c1c0e215a31258e75c1
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-19 15:24:40 +0200

    Fixed AboutMe

--- src/components/main/aboutMe/AboutMe.tsx
+++ src/components/main/aboutMe/AboutMe.tsx
@@ -4,17 +4,13 @@ function AboutMe(props) {
 
     return (
         <section id="aboutMe">
-            <div className="container dynamic-flex">
-                <div className="top">
-                    <h1 className="title">About Me</h1>
-                    <h3 className="subtitle">My Introduction</h3>
-                </div>
+            <div className="void-space"></div>
 
-                <div className="bottom">
-                    <div className="image"></div>
-                    <div className="description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad commodi laboriosam quia rem veniam.</p>
-                    </div>
+            <div className="container dynamic-flex">
+                <div className="image"></div>
+                <div className="description">
+                    <h5>"Lorem ipsum dolor sit amet, consectetur adipisicing."</h5>
+                    <p>"Appassionato di tecnologia in tutti i suoi ambiti e costantemente aggiornato sulle novità introdotte. Sono una persona socievole, organizzata e attenta ai dettagli con l'obbiettivo di raggiungere sempre il miglior risultato possibile. ll mondo della programmazione è in continua evoluzione e sapere che avrò sempre qualcosa da imparare mi motiva molto. Non vedo l'ora di poter lavorare a nuovi progetti, approfondire la conoscenza dei linguaggi che ho studiato e apprendere nuove tecnologie."</p>
                 </div>
             </div>
         </section>
