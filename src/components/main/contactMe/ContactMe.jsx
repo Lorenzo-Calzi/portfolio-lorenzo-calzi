8cd5aaebb901f1764b3a237742da694707308843
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-25 19:44:19 +0200

    Added form, emailJs, fixed layout and responsive

--- src/components/main/contactMe/ContactMe.jsx
+++ src/components/main/contactMe/ContactMe.jsx
@@ -1,15 +1,19 @@
 import * as React from "react"
+import {Form, VoidSpace} from "../../imports";
 
 function ContactMe() {
 
     return (
         <section id="contactMe">
-            <div className="void-space"></div>
+            {/*<VoidSpace />*/}
 
             <div className="container dynamic-flex">
-                <div className="description">
-                    <h5>"Contacts"</h5>
+                <div className="titles">
+                    <h1 className="title-gradient">Contatti</h1>
+                    <h2 className="title-solid">Contatti</h2>
                 </div>
+
+                <Form/>
             </div>
         </section>
     )
