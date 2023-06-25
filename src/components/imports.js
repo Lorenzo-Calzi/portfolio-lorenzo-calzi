8ac818b39a6e1751f7b56f99875e5ba377c22996
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-25 19:46:28 +0200

    Added imports

--- src/components/imports.js
+++ src/components/imports.js
@@ -8,4 +8,8 @@ export { default as Main } from "./main/Main";
 export { default as AboutMe } from "./main/aboutMe/AboutMe.tsx";
 export { default as Skills } from "./main/skills/Skills";
 export { default as ContactMe } from "./main/contactMe/ContactMe";
+export { default as Form } from "./main/contactMe/form/Form.tsx";
 export { default as Projects } from "./main/projects/Projects";
+export { default as Carousel } from "./main/projects/carousel/Carousel";
+
+export { default as VoidSpace } from "./void-space/VoidSpace.tsx";
