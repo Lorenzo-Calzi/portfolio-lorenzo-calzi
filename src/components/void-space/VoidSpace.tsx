2c945ff0f8bca78170f60f84607723ac9c88e570
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-25 19:45:58 +0200

    Added void space component

---
+++ src/components/void-space/VoidSpace.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Marquee from "react-fast-marquee";
+
+function VoidSapce() {
+
+    return (
+        <div className="void-space">
+            <Marquee>
+                <i className="fab fa-github"></i>
+                <i className="fab fa-codepen"></i>
+                <i className="fab fa-free-code-camp"></i>
+                <i className="fab fa-dev"></i>
+                <i className="fab fa-react"></i>
+                <i className="fab fa-vuejs"></i>
+                <i className="fab fa-angular"></i>
+                <i className="fab fa-node"></i>
+                <i className="fab fa-wordpress"></i>
+                <i className="fab fa-aws"></i>
+                <i className="fab fa-docker"></i>
+                <i className="fab fa-android"></i>
+            </Marquee>
+        </div>
+    )
+}
+
+export default VoidSapce;
