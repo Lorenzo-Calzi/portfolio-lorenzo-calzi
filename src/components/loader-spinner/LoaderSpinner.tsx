8903f941bf2c3a41b09c80d65ca9d0ca8ff27c4f
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-28 15:57:14 +0200

    Added component

---
+++ src/components/loader-spinner/LoaderSpinner.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import {BarLoader} from "react-spinners";
+
+function LoaderSpinner(props) {
+
+    return (
+        <>
+            {
+                (props.loading.bar || props.loading.popup) && (
+                    <div className="loader-container">
+                        {
+                            props.loading.bar && (
+                                <BarLoader color={'#6D56E9'} loading={props.loading.bar} className="loader-spinner" height={15} width={200} speedMultiplier={1}/>
+                            )
+                        }
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default LoaderSpinner;
