ed982c4fc93ff6bf538e026cb482145988cf7567
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-06-28 15:56:45 +0200

    Fixed component, fixed css, added pdf

--- src/components/main/aboutMe/AboutMe.tsx
+++ src/components/main/aboutMe/AboutMe.tsx
@@ -19,6 +19,7 @@ function AboutMe(props) {
                     <div className="description">
                         <h5>Vi racconto un po' di me</h5>
                         <p>"Appassionato di tecnologia in tutti i suoi ambiti e costantemente aggiornato sulle novità introdotte. Sono una persona socievole, organizzata e attenta ai dettagli con l'obbiettivo di raggiungere sempre il miglior risultato possibile. ll mondo della programmazione è in continua evoluzione e sapere che avrò sempre qualcosa da imparare mi motiva molto. Non vedo l'ora di poter lavorare a nuovi progetti, approfondire la conoscenza dei linguaggi che ho studiato e apprendere nuove tecnologie."</p>
+                        <a href={process.env.PUBLIC_URL + '/assets/cv/cv_lorenzo_calzi.pdf'} download>DOWNLOAD CV</a>
                     </div>
                 </div>
             </div>
